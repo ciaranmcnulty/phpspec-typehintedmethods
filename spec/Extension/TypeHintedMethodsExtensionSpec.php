9f758e0b64817497df53b01891cdd06279c268f2
James Halsall <james.halsall@inviqa.com>
2016-08-12 10:15:46 +0100

    Update extension for phpspec ~3.0

--- spec/Extension/TypeHintedMethodsExtensionSpec.php
+++ spec/Extension/TypeHintedMethodsExtensionSpec.php
@@ -2,7 +2,7 @@
 
 namespace spec\Cjm\PhpSpec\Extension;
 
-use PhpSpec\CodeGenerator\Generator\MethodGenerator;
+use PhpSpec\Extension;
 use PhpSpec\ObjectBehavior;
 use PhpSpec\ServiceContainer;
 use Prophecy\Argument;
@@ -11,13 +11,13 @@ class TypeHintedMethodsExtensionSpec extends ObjectBehavior
 {
     function it_is_a_phpspec_extension()
     {
-        $this->shouldHaveType('PhpSpec\Extension\ExtensionInterface');
+        $this->shouldHaveType(Extension::class);
     }
 
     function it_registers_the_replacement_method_generator(ServiceContainer $container)
     {
-        $this->load($container);
+        $this->load($container, []);
 
-        $container->set('code_generator.generators.method', Argument::any())->shouldHaveBeenCalled();
+        $container->define('code_generator.generators.method', Argument::any())->shouldHaveBeenCalled();
     }
 }
